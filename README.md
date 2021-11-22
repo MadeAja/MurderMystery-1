06f58639d33c5840c09b678307ae93ddf840cb70
DEVS <83214438+DanteDEVS@users.noreply.github.com>
2021-11-22 20:45:36 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,14 @@
+# MurderMystery
+A PMMP MINIGAME MURDERMYSTERY
+CREDITS TO THE OWNER: LEYKEY
+# FEATURES
+- THROWABLE SWORD
+- GOLD SPAWNS
+- SCOREBOARD
+- BOSSBAR
+# V3 UPDATES:
+- HYPIXEL MEASAGES
+- BOSSBAR CHANGE
+- ADDDED {restartt} for the scoreboard
+- restartt is the restartime
+------------------------------------
---
+++ src/mm/MMinfo.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+
+namespace mm;
+
+use pocketmine\Plugin;
+
+class MMinfo extends Info{
+	public const NAME = "MurderMystery";
+	public const VERSION = "V3";
+	public const BUILD_NUMBER = "?";
+        public const DESCRIPTION = "A MINIGAME PLUGIN MURDERMYSTERY";
+        public const API = "3.0.0";
+        public const TYPE = "MINIGAMES, FUN";
+        public const AUTHOR = "LEYKEY, DANTEDEVS";
+    }
+}
